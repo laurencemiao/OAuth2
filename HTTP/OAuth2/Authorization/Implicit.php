fcf58a4360d63ac95ab3693cea50ae98cc1c7c2b
Laurence Miao <mf@mf.(none)>
2011-04-15 17:02:37 +0800

    initial refactoring for draft-15

---
+++ HTTP/OAuth2/Authorization/Implicit.php
@@ -0,0 +1,12 @@
+<?php
+
+class HTTP_OAuth2_Token_AuthorizationCode{
+    public $client_id       = null;
+    public $username        = null;
+    public $code            = null;
+    public $redirect_uri    = null;
+    public $scope           = null;
+    
+    public $authorization_id = null;
+}
+
