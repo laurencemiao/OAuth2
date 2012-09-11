d4bf7a47e3bffcbd01c7b5b8d9af2771f4bbc41d
Laurence Miao <laurence.miao@gmail.com>
2012-09-11 16:45:05 +0800

    removed Assertion grant type

--- HTTP/OAuth2/Authorization/Assertion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'HTTP/OAuth2/Authorization/Abstract.php';
-
-class HTTP_OAuth2_Authorization_Assertion extends HTTP_OAuth2_Authorization_Abstract{
-    public $assertion_type = null;
-    public $assertion = null;
-}
-
