6cae9f8c2a63af7a116ca553612eca477c3f864b
Laurence Miao <laurence.miao@gmail.com>
2012-09-11 18:12:22 +0800

    added class Redirection

---
+++ HTTP/OAuth2/Server/EndPoint/Redirection.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'HTTP/OAuth2/Server/EndPoint/Abstract.php';
+
+class HTTP_OAuth2_Server_EndPoint_Redirection extends HTTP_OAuth2_Server_EndPoint_Abstract{
+
+    function handle(){
+    }
+}
