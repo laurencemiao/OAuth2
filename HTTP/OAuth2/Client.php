f3e49d3834af8768ccff788599550a5b01c89dde
Laurence Miao <laurence.miao@gmail.com>
2012-09-11 17:21:05 +0800

    added client identifier property

--- HTTP/OAuth2/Client.php
+++ HTTP/OAuth2/Client.php
@@ -7,6 +7,7 @@ abstract class HTTP_OAuth2_Client extends HTTP_OAuth2{
     const CLIENT_TYPE_CONFIDENTIAL	= 'confidential';
     const CLIENT_TYPE_PUBLIC		= 'public';
 
+    public $client_identifier		= null;
     public $type			= null;
     public $redirect_uri		= null;
 }
