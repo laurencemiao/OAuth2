33a56f7a7f4d34a433a3002d15439e3bda864aad
Laurence Miao <laurence.miao@gmail.com>
2012-09-27 18:40:41 +0800

    changed client_id

--- HTTP/OAuth2/Client.php
+++ HTTP/OAuth2/Client.php
@@ -7,9 +7,35 @@ abstract class HTTP_OAuth2_Client extends HTTP_OAuth2{
     const CLIENT_TYPE_CONFIDENTIAL	= 'confidential';
     const CLIENT_TYPE_PUBLIC		= 'public';
 
-    public $client_identifier		= null;
+    public $client_id			= null;
     public $type			= null;
-    public $redirect_uri		= null;
+    /**
+     * QUOTED FROM draft-ietf-oauth-v2-31,
+     *
+     * 3.1.2.2. Registration Requirements
+     *
+     * The authorization server MUST require the following clients to
+     * register their redirection endpoint:
+     *
+     * o  Public clients.
+     * o  Confidential clients utilizing the implicit grant type.
+     *
+     * ...
+     * The authorization server MAY allow the client to register multiple
+     * redirection endpoints.
+     */
+    public $registered_redirect_uris	= array();
 
+    /**
+     * QUOTED FROM draft-ietf-oauth-v2-31,
+     *
+     * 3.1.2.3.  Dynamic Configuration
+     *
+     * If multiple redirection URIs have been registered, if only part of
+     * the redirection URI has been registered, or if no redirection URI has
+     * been registered, the client MUST include a redirection URI with the
+     * authorization request using the "redirect_uri" request parameter.
+     */
+    public $redirect_uri		= null;
     public $credentials			= null;
 }
--- HTTP/OAuth2/Token/RefreshToken.php
+++ HTTP/OAuth2/Token/RefreshToken.php
@@ -3,6 +3,19 @@
 
 class HTTP_OAuth2_Token_RefreshToken{
 
+    /**
+     * QUOTED FROM draft-ietf-oauth-v2-31,
+     *
+     * 3.2.1.  Client Authentication
+     *
+     *
+     * o  Enforcing the binding of refresh tokens and authorization codes to
+     * the client they were issued to.  Client authentication is critical
+     * when an authorization code is transmitted to the redirection
+     * endpoint over an insecure channel, or when the redirection URI has
+     * not been registered in full.
+     */
+    public $client_id = null;
     public $redirect_uri = null;
     
     public $authorization_id = null;
